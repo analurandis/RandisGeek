c98a0b3675a611b6c557acc8122df96b6b517759
Ana Lucia dos Santos <ana.lucia.dos.santos@avanade.com>
2023-04-05 11:46:09 -0300

    aplicação console uso service bus

---
+++ GeekBus/Program.cs
@@ -0,0 +1,44 @@
+﻿using GeekBus.Service;
+
+ServiceBus serviceBus = new ServiceBus();
+
+Console.WriteLine("Uso do Service Bus");
+
+int escolha = 0;
+do
+{
+    Console.WriteLine("Escolha uma opção:");
+    Console.WriteLine("1. Envio de Mensagem");
+    Console.WriteLine("2. Recebimento de Mensagem");
+    Console.WriteLine("0. Sair");
+    Console.Write("Opção escolhida: ");
+    try
+    {
+        escolha = int.Parse(Console.ReadLine());
+    }
+    catch
+    {
+        Console.WriteLine("Opção inválida. Tente novamente.");
+        continue;
+    }
+    switch (escolha)
+    {
+        case 1:
+            Console.WriteLine("Envio de Mensagem");
+            Console.WriteLine("Digite a mensagem desejada:");
+            var mensagem = Console.ReadLine();
+            serviceBus.SendMessage(mensagem);
+            break;
+        case 2:
+            Console.WriteLine("Recebimento de Mensagem");
+            serviceBus.ReceivedMessage();
+            break;
+        case 0:
+            Console.WriteLine("Saindo do programa...");
+            break;
+        default:
+            Console.WriteLine("Opção inválida. Tente novamente.");
+            break;
+    }
+    Console.WriteLine();
+} while (escolha != 0);
---
+++ GeekBus/Service/ServiceBus.cs
@@ -0,0 +1,79 @@
+﻿using Azure.Core;
+using Azure.Messaging.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekBus.Service
+{
+    public class ServiceBus
+    {
+        private readonly string connectionString = "Endpoint=sb://randisgeekburguer.servicebus.windows.net/;SharedAccessKeyName=ProductPolicy;SharedAccessKey=uebbznq6EIovHsH7Nq9vnU6oGfvoUxP7m+ASbLrL5jI=";
+        private readonly string queueName = "productchanged";
+
+
+        public async Task SendMessage(string mensagem)
+        {
+            await using var client = new ServiceBusClient(connectionString);
+            ServiceBusSender sender = client.CreateSender(queueName);
+            ServiceBusMessage message = new ServiceBusMessage(mensagem);
+
+            await sender.SendMessageAsync(message);
+
+
+            ServiceBusReceiver receiver = client.CreateReceiver(queueName);
+            ServiceBusReceivedMessage receivedMessage = await receiver.ReceiveMessageAsync();
+            string body = receivedMessage.Body.ToString();
+            Console.WriteLine(body);
+        }
+
+        public async Task ReceivedMessage()
+        {
+            await using var client = new ServiceBusClient(connectionString);
+
+            ServiceBusProcessor processor;
+            processor = client.CreateProcessor("ProductChanged", new ServiceBusProcessorOptions());
+
+            try
+            {
+
+                processor.ProcessMessageAsync += MessageHandler;
+                processor.ProcessErrorAsync += ErrorHandler;
+                await processor.StartProcessingAsync();
+
+                Console.WriteLine("Wait for a minute and then press any key to end the processing");
+                Console.ReadKey();
+
+                // stop processing 
+                Console.WriteLine("\nStopping the receiver...");
+                await processor.StopProcessingAsync();
+                Console.WriteLine("Stopped receiving messages");
+            }
+            finally
+            {
+                await processor.DisposeAsync();
+                await client.DisposeAsync();
+            }
+
+        }
+
+
+        private async Task MessageHandler(ProcessMessageEventArgs args)
+        {
+            string body = args.Message.Body.ToString();
+            Console.WriteLine($"Received: {body}");
+
+            await args.CompleteMessageAsync(args.Message);
+        }
+
+        private Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+
+    }
+}
